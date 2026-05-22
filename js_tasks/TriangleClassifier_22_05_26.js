c5dbae180ce3530ba1426ef86b8f14625aae898b
sratnakumar-gollapalli <sratnakumargollapalli@gmail.com>
2026-05-22 10:49:15 +0530

    Added type validation

--- js_tasks/TriangleClassifier_22_05_26.js
+++ js_tasks/TriangleClassifier_22_05_26.js
@@ -5,23 +5,40 @@
  * isosceles (exactly two sides are equal), or scalene (no sides are equal).
  * Use an if-else statement to classify the triangle.
  */
+{
+// Type checking
+// let side_A = 'hellow'
+// let side_B = false
+// let side_C = 50
 
+// equilateral (all sides are equal)
 // let side_A = 40
 // let side_B = 40
 // let side_C = 40
 
+//isosceles (exactly two sides are equal
 // let side_A = 40
 // let side_B = 40
 // let side_C = 50
 
+// scalene (no sides are equal)
 let side_A = 30
 let side_B = 40
 let side_C = 50
 
-if(side_A === side_B && side_B === side_C){
+if ( typeof side_A === 'number' && typeof side_A === 'number' && typeof side_C === 'number'){
+ if(side_A === side_B && side_B === side_C){
    console.log('Equilateral (all sides are equal)')
-}else if(side_A === side_B || side_B === side_C || side_A === side_C ){
+ }else if(side_A === side_B || side_B === side_C || side_A === side_C ){
    console.log('Isosceles (exactly two sides are equal)')
-}else{
+ }else{
     console.log('Scalene (no sides are equal)')
+ }
+}else{
+   console.log("Verify the type of the current values")
+   console.log(`Type of side_A : ${typeof side_A}`)
+   console.log(`Type of side_A : ${typeof side_B}`)
+   console.log(`Type of side_A : ${typeof side_C}`)
+}
+
 }
